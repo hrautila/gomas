463a6afbb3e7066cf177eb46666900d3bf9c9311
Harri Rautila <harri.rautila@gmail.com>
2014-05-31 01:14:04 +0300

    Tests for TRDMult().

--- lapackd/test/trired_test.go
+++ lapackd/test/trired_test.go
@@ -13,6 +13,7 @@ import (
     "github.com/hrautila/gomas/blasd"
     "github.com/hrautila/gomas/lapackd"
     "testing"
+    //"fmt"
 )
 
 func TestTriRedLower(t *testing.T) {
@@ -81,6 +82,210 @@ func TestTriRedUpper(t *testing.T) {
     nrm = blasd.Nrm2(tau)
     t.Logf("   ||unblk.Trired(tau) - blk.Trired(tau)||_1: %e\n", nrm)
 }
+
+func TestTrdMultLower(t *testing.T) {
+    var dt, et, da, ea cmat.FloatMatrix
+    N := 843
+    nb := 48
+    conf := gomas.NewConf()
+    conf.LB = nb
+
+    A := cmat.NewMatrix(N, N)
+    tau := cmat.NewMatrix(N, 1)
+    src := cmat.NewFloatNormSource()
+    // create symmetric matrix
+    A.SetFrom(src, cmat.SYMM)
+    A0 := cmat.NewCopy(A)
+
+    W := lapackd.Workspace(lapackd.TRDReduceWork(A, conf))
+    lapackd.TRDReduce(A, tau, W, gomas.LOWER, conf)
+
+    // make tridiagonal matrix T
+    T0 := cmat.NewMatrix(N, N)
+    dt.Diag(T0)
+    da.Diag(A)
+    blasd.Copy(&dt, &da)
+    
+    ea.Diag(A, -1)
+    et.Diag(T0, -1)
+    blasd.Copy(&et, &ea)
+    et.Diag(T0, 1)
+    blasd.Copy(&et, &ea)
+    T1 := cmat.NewCopy(T0)
+
+    // compute Q*T*Q.T (unblocked)
+    conf.LB = 0
+    lapackd.TRDMult(T0, A, tau, W, gomas.LEFT|gomas.LOWER, conf)
+    lapackd.TRDMult(T0, A, tau, W, gomas.RIGHT|gomas.TRANS|gomas.LOWER, conf)
+
+    blasd.Plus(T0, A0, 1.0, -1.0, gomas.NONE)
+    nrm := lapackd.NormP(T0, lapackd.NORM_ONE)
+    t.Logf("N=%d, unblk.||A - Q*T*Q.T||_1: %e\n", N, nrm)
+    
+    // compute Q*T*Q.T (blocked)
+    conf.LB = nb
+    lapackd.TRDMult(T1, A, tau, W, gomas.LEFT|gomas.LOWER, conf)
+    lapackd.TRDMult(T1, A, tau, W, gomas.RIGHT|gomas.TRANS|gomas.LOWER, conf)
+
+    blasd.Plus(T1, A0, 1.0, -1.0, gomas.NONE)
+    nrm = lapackd.NormP(T1, lapackd.NORM_ONE)
+    t.Logf("N=%d,   blk.||A - Q*T*Q.T||_1: %e\n", N, nrm)
+}
+
+
+func TestTrdMultUpper(t *testing.T) {
+    var dt, et, da, ea cmat.FloatMatrix
+    N := 843
+    nb := 48
+    conf := gomas.NewConf()
+    conf.LB = nb
+
+    A := cmat.NewMatrix(N, N)
+    tau := cmat.NewMatrix(N, 1)
+    src := cmat.NewFloatNormSource()
+    // create symmetric matrix
+    A.SetFrom(src, cmat.SYMM)
+    A0 := cmat.NewCopy(A)
+
+    W := lapackd.Workspace(lapackd.TRDReduceWork(A, conf))
+    lapackd.TRDReduce(A, tau, W, gomas.UPPER, conf)
+    
+    // make tridiagonal matrix T
+    T0 := cmat.NewMatrix(N, N)
+    dt.Diag(T0)
+    da.Diag(A)
+    blasd.Copy(&dt, &da)
+    
+    ea.Diag(A, 1)
+    et.Diag(T0, 1)
+    blasd.Copy(&et, &ea)
+    et.Diag(T0, -1)
+    blasd.Copy(&et, &ea)
+    T1 := cmat.NewCopy(T0)
+
+    // compute Q*T*Q.T (unblocked)
+    conf.LB = 0
+    lapackd.TRDMult(T0, A, tau, W, gomas.LEFT|gomas.UPPER, conf)
+    lapackd.TRDMult(T0, A, tau, W, gomas.RIGHT|gomas.TRANS|gomas.UPPER, conf)
+
+    blasd.Plus(T0, A0, 1.0, -1.0, gomas.NONE)
+    nrm := lapackd.NormP(T0, lapackd.NORM_ONE)
+    t.Logf("N=%d, unblk.||A - Q*T*Q.T||_1: %e\n", N, nrm)
+    
+    // compute Q*T*Q.T (blocked)
+    conf.LB = nb
+    W = lapackd.Workspace(lapackd.TRDMultWork(A, gomas.LEFT|gomas.UPPER, conf))
+    lapackd.TRDMult(T1, A, tau, W, gomas.LEFT|gomas.UPPER, conf)
+    lapackd.TRDMult(T1, A, tau, W, gomas.RIGHT|gomas.TRANS|gomas.UPPER, conf)
+
+    blasd.Plus(T1, A0, 1.0, -1.0, gomas.NONE)
+    nrm = lapackd.NormP(T1, lapackd.NORM_ONE)
+    t.Logf("N=%d,   blk.||A - Q*T*Q.T||_1: %e\n", N, nrm)
+}
+
+
+// test: ||T - Q.T*A*Q|| == 0
+func TestTrdMultALower(t *testing.T) {
+    var dt, et, da, ea cmat.FloatMatrix
+    N := 843
+    nb := 48
+    conf := gomas.NewConf()
+    conf.LB = nb
+
+    A := cmat.NewMatrix(N, N)
+    tau := cmat.NewMatrix(N, 1)
+    src := cmat.NewFloatNormSource()
+    // create symmetric matrix
+    A.SetFrom(src, cmat.SYMM)
+    A0 := cmat.NewCopy(A)
+    A1 := cmat.NewCopy(A)
+
+    W := lapackd.Workspace(lapackd.TRDReduceWork(A, conf))
+    lapackd.TRDReduce(A, tau, W, gomas.LOWER, conf)
+
+    // make tridiagonal matrix T
+    T0 := cmat.NewMatrix(N, N)
+    dt.Diag(T0)
+    da.Diag(A)
+    blasd.Copy(&dt, &da)
+    
+    ea.Diag(A, -1)
+    et.Diag(T0, -1)
+    blasd.Copy(&et, &ea)
+    et.Diag(T0, 1)
+    blasd.Copy(&et, &ea)
+
+    // compute Q.T*A*Q (unblocked)
+    conf.LB = 0
+    lapackd.TRDMult(A0, A, tau, W, gomas.LEFT|gomas.TRANS|gomas.LOWER, conf)
+    lapackd.TRDMult(A0, A, tau, W, gomas.RIGHT|gomas.LOWER, conf)
+
+    blasd.Plus(A0, T0, 1.0, -1.0, gomas.NONE)
+    nrm := lapackd.NormP(A0, lapackd.NORM_ONE)
+    t.Logf("N=%d, unblk.||T - Q.T*A*Q||_1: %e\n", N, nrm)
+    
+    // compute Q.T*A*Q (blocked)
+    conf.LB = nb
+    lapackd.TRDMult(A1, A, tau, W, gomas.LEFT|gomas.TRANS|gomas.LOWER, conf)
+    lapackd.TRDMult(A1, A, tau, W, gomas.RIGHT|gomas.LOWER, conf)
+
+    blasd.Plus(A1, T0, 1.0, -1.0, gomas.NONE)
+    nrm = lapackd.NormP(A1, lapackd.NORM_ONE)
+    t.Logf("N=%d,   blk.||T - Q.T*A*Q||_1: %e\n", N, nrm)
+}
+
+// test: ||T - Q.T*A*Q|| == 0
+func TestTrdMultAUpper(t *testing.T) {
+    var dt, et, da, ea cmat.FloatMatrix
+    N := 843
+    nb := 48
+    conf := gomas.NewConf()
+    conf.LB = nb
+
+    A := cmat.NewMatrix(N, N)
+    tau := cmat.NewMatrix(N, 1)
+    src := cmat.NewFloatNormSource()
+    // create symmetric matrix
+    A.SetFrom(src, cmat.SYMM)
+    A0 := cmat.NewCopy(A)
+    A1 := cmat.NewCopy(A)
+
+    W := lapackd.Workspace(lapackd.TRDReduceWork(A, conf))
+    lapackd.TRDReduce(A, tau, W, gomas.UPPER, conf)
+
+    // make tridiagonal matrix T
+    T0 := cmat.NewMatrix(N, N)
+    dt.Diag(T0)
+    da.Diag(A)
+    blasd.Copy(&dt, &da)
+    
+    ea.Diag(A, 1)
+    et.Diag(T0, 1)
+    blasd.Copy(&et, &ea)
+    et.Diag(T0, -1)
+    blasd.Copy(&et, &ea)
+
+    // compute Q.T*A*Q (unblocked)
+    conf.LB = 0
+    lapackd.TRDMult(A0, A, tau, W, gomas.LEFT|gomas.TRANS|gomas.UPPER, conf)
+    lapackd.TRDMult(A0, A, tau, W, gomas.RIGHT|gomas.UPPER, conf)
+
+    blasd.Plus(A0, T0, 1.0, -1.0, gomas.NONE)
+    nrm := lapackd.NormP(A0, lapackd.NORM_ONE)
+    t.Logf("N=%d, unblk.||T - Q.T*A*Q||_1: %e\n", N, nrm)
+    
+    // compute Q.T*A*Q (blocked)
+    conf.LB = nb
+    lapackd.TRDMult(A1, A, tau, W, gomas.LEFT|gomas.TRANS|gomas.UPPER, conf)
+    lapackd.TRDMult(A1, A, tau, W, gomas.RIGHT|gomas.UPPER, conf)
+
+    blasd.Plus(A1, T0, 1.0, -1.0, gomas.NONE)
+    nrm = lapackd.NormP(A1, lapackd.NORM_ONE)
+    t.Logf("N=%d,   blk.||T - Q.T*A*Q||_1: %e\n", N, nrm)
+}
+
+
+
 
 // Local Variables:
 // tab-width: 4
